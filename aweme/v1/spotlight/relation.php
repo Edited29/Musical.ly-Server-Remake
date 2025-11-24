828bd55d187090d840a2abbd9c3b4e3c90aecbc7
Edited29 <rhauldejesus3@gmail.com>
2025-11-23 17:02:55 -0800

    updated

---
+++ aweme/v1/spotlight/relation.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+?>
+{
+  "status_code": 0,
+  "status_msg": "",
+  "extra": {
+    "fatal_item_ids": [],
+    "now": 1710000000000,
+    "logid": "20250101000000000000000000000000"
+  },
+  "spotlight_list": [
+    {
+      "id": "1001",
+      "title": "Title",
+      "desc": "Description",
+      "cover": {
+        "url_list": [
+          "https://p16-sign-sg.tiktokcdn.com/obj/placeholder1.jpeg"
+        ]
+      },
+      "schema": "aweme://user/profile/1"
+    }
+  ]
+}
