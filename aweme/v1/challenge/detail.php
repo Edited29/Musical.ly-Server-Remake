d8bf1c39202d4bc4b970648e3e1400976b538a2b
Edited29 <rhauldejesus3@gmail.com>
2025-11-23 17:04:28 -0800

    updated

---
+++ aweme/v1/challenge/detail.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+?>
+{
+  "status_code": 0,
+  "status_msg": "",
+  "challenge_info": {
+    "cid": "9999",
+    "cha_name": "challenge",
+    "desc": "Description",
+    "schema": "",
+    "view_count": 123456,
+    "is_commerce": false,
+    "cover_item": {
+      "url_list": [
+        "https://p16-sign-va.tiktokcdn.com/obj/placeholder2.jpeg"
+      ]
+    }
+  },
+  "extra": {
+    "now": 1710000000100,
+    "logid": "20250101000000000000000000000001"
+  }
+}
